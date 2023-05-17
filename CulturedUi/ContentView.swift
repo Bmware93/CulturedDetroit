9b479bb0ea19db2d47ac0dc2450c23c032fd5df2
Benia Morgan <bmorganware@gmail.com>
2023-05-17 13:34:40 -0400

    Initial Commit

---
+++ CulturedUi/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CulturedUi
+//
+//  Created by Benia Morgan-Ware on 5/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CulturedUi/CulturedUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CulturedUiApp.swift
+//  CulturedUi
+//
+//  Created by Benia Morgan-Ware on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct CulturedUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
