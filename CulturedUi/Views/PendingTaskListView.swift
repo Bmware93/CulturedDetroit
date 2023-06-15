b17b23f177ecb29019f7a082f9b48fca411e3528
Omar D <omardavidson227@gmail.com>
2023-06-15 13:24:13 -0400

    Reverting original changes to the Activities List.

--- CulturedUi/Views/PendingTaskListView.swift
+++ CulturedUi/Views/PendingTaskListView.swift
@@ -14,34 +14,33 @@ struct PendingTaskListView: View {
         NavigationView {
             VStack {
                 ScrollView {
-                    ForEach(vm.tasks.indices, id: \.self) { index in
+                    ForEach(vm.tasks, id: \.self) { task in
                         HStack(alignment: .center) {
-                            #warning("the index has no member for imageName")
-//                            Image(index.imageName)
-//                                .resizable()
-//                                .aspectRatio(contentMode: .fit)
-//                                .cornerRadius(10)
-//                                .frame(width: 150, height: 150)
-//                                .padding(.trailing)
-//                                .shadow(radius: 3 , x: 2, y: 2)
+                            Image(task.imageName)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .cornerRadius(10)
+                                .frame(width: 150, height: 150)
+                                .padding(.trailing)
+                                .shadow(radius: 3 , x: 2, y: 2)
                             VStack(alignment: .leading) {
-                                Text(index.description)
+                                Text(task.description)
                                     .font(.system(size: fontSize))
 
                                     .minimumScaleFactor(0.5) // Adjust as needed
                                     .lineLimit(1)
                                 
                                 Button {
-//                                    _Concurrency.Task {
-//                                        do {
-                                    #warning("index isn't working with the search term input for the api call.")
-//                                            try await vm.businesses(searchingFor: task.searchTerm, at: vm.mapRegion.center)
-//                                        } catch {
-//                                            print(error.localizedDescription)
-//                                        }
-//
-//                                    }
-                                    vm.completeTask(index: index)
+                                    _Concurrency.Task {
+                                        do {
+                                            
+                                            try await vm.businesses(searchingFor: task.searchTerm, at: vm.mapRegion.center)
+                                        } catch {
+                                            print(error.localizedDescription)
+                                        }
+                                        
+                                    }
+                                    
                                 } label: {
                                     Text("Search")
 
